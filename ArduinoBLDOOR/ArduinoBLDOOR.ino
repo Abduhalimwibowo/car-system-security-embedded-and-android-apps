014db4235f34f5a16a212e30ef9bc4270fffaff1
Abduhalimwibowo <ryuzaki.halim@gmail.com>
2017-11-14 09:33:14 +0700

    Here's the Aduino Code to the Acuator
    Here's the Aduino Code to the Acuator and Module GSM()

---
+++ ArduinoBLDOOR/ArduinoBLDOOR.ino
@@ -0,0 +1,99 @@
+#include <SoftwareSerial.h>
+#include <stdio.h>
+
+//9 DOOR
+//10 BUZZER
+int doorONE = 9;
+int doorTWO = 8;
+
+
+int bluetoothTX = 11 ;
+int bluetoothRX = 10 ;
+char receivedValue ;
+
+SoftwareSerial bluetooth ( bluetoothTX, bluetoothRX );
+
+void setup()
+{
+  Serial.begin(9600);  
+  Serial.println("console> ");
+  
+  pinMode(doorONE, OUTPUT);
+  pinMode(doorTWO, OUTPUT);
+ 
+  bluetooth.begin(115200);
+  bluetooth.print("$$$");
+  delay(100);
+  bluetooth.println("U,9600,N");
+  bluetooth.begin(9600);
+
+}
+
+
+void loop()
+{
+int power = 0;
+int door = 0;
+signed int data = 0;
+
+ if( bluetooth.available() )
+  {
+    data = (int) bluetooth.read();
+    Serial.println( data );                 // for debugging, show received data
+
+      if(data == 26) 
+      {
+        allDown() ;
+      }else if(data == 89)
+      {
+        allUp() ;
+      }else{
+        
+      door = data / 100;                     // which door to select ?
+      power = data % 100 ;             // 0 - 25 , DOOR power ( * 10 ) for actual value
+           
+    switch(door)                             // Now, let's select the right door.
+    {
+      
+      case 0  : setDOORtwo ( power * 10 );  break;
+      case 1  : setDOORone ( power * 10 );    break;
+      default : setDOORtwo ( power * 10 );  break;       // DO NOT know what to do ? must be door 8
+      
+    }
+      }
+
+    bluetooth.flush();                       // IMPORTANT clean bluetooth stream, flush stuck data
+
+  }
+
+}
+
+
+// SHUT DOWN all door
+void allDown()
+{
+    digitalWrite(doorONE,  LOW ) ; 
+      digitalWrite(doorTWO,  LOW ) ; 
+}
+
+// ALL up now :)
+
+void allUp()
+{
+    digitalWrite(doorONE,  HIGH) ;  
+     digitalWrite(doorTWO,  HIGH ) ; 
+}
+
+
+void setDOORone(int powerss)
+{
+ analogWrite(doorONE,  powerss ) ; 
+}
+
+
+void setDOORtwo(int powerss)
+{
+ analogWrite(doorTWO,  powerss ) ; 
+}
+
+
---
+++ ArduinoGSMFP/ArduinoGSMFP.ino
@@ -0,0 +1,151 @@
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+int getFingerprintIDez();
+
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+SoftwareSerial mySerial(2, 3);
+SoftwareSerial gsm(5, 6);
+
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  gsm.begin(9600);
+  Serial.begin(9600);
+  Serial.println("fingertest");
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    //Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  //Serial.println("Waiting for valid finger...");
+}
+
+void loop()                     // run over and over again
+{
+  if (gsm.available()>0)
+  Serial.write(gsm.read());
+  getFingerprintIDez();
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(50);            //don't ned to run this at full speed.
+
+
+}
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+     // Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      //Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+     // Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      //Serial.println("Imaging error");
+      return p;
+    default:
+      //Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+    //  Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+     // Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+     // Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+     // Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+     // Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+     // Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+  delay(1000);
+  Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  //Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  //Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  
+ {
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(500);
+  Serial.println("Someone on your Car, We sent SMS !");
+  gsm.println("AT");
+  delay(1000);
+  gsm.println("AT+CMGF=1");    //Sent SMS dengan mode text
+  delay(1000);
+  gsm.println("AT+CMGS=\"+628996299048\"\r"); // Tujuan SMS diarahkan ke no telepon pengemudi
+  delay(1000);
+  gsm.println("Someone on your Car");//Isi pesan
+  delay(1000);
+  gsm.println((char)26);//kode ASCII code dari ctrl+z yakni 26
+  delay(1000);
+  return -1;
+  }
+  
+  // found a match!
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+  delay(1000);
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+  
+}
